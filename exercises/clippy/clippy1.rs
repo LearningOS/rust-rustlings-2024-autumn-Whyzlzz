783983b9e4da7f5a08d65ea3491c30a13f720fe8
Whyzlzz <2746537593@qq.com>
2024-10-04 10:50:22 +0800

    solve

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -11,10 +11,11 @@
 
 // I AM NOT DONE
 
+use core::f32;
 use std::f32;
 
 fn main() {
-    let pi = 3.14f32;
+    let pi = f32::consts::PI ;
     let radius = 5.00f32;
 
     let area = pi * f32::powi(radius, 2);
